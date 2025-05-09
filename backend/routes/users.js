17f37418a2a75b41728ca23fb8c335f792ccd222
1990OKTexas <jamesmaddocks90@gmail.com>
2025-05-09 01:13:10 -0500

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,1 @@
-import express from 'express';
-const router = express.Router();
 
-// Example route
-router.get('/', (req, res) => {
-  res.send('Users route working!');
-});
-
-// Add your user-related routes here
-
-export default router;
